24ad095757a3b1c7d7afa4559de86ac427738efd
Ahad <72428146+Ahadkhan-12@users.noreply.github.com>
2023-04-10 19:49:26 +0530

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
-import { Rating } from "@material-ui/lab/";
+import { Rating } from "@material-ui/lab/"; 
 
 import useStyles from "./styles";
 
