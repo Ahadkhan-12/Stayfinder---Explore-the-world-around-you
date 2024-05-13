f4d2f5d3b086e908a69f601e308eac38a6db5c8a
Ahad <72428146+Ahadkhan-12@users.noreply.github.com>
2024-05-13 23:26:04 +0530

    Add files via upload

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,27 @@
 import axios from "axios";
 
-export const getPlacesData = async (type, sw, ne)=> {
-    try{
-        const {data: {data}} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
-          params: {
-            bl_latitude: sw.lat,
-            tr_latitude: ne.lat,
-            bl_longitude: sw.lng,
-            tr_longitude: ne.lng,
-          },
-          headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY2,
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-          }
-        });
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
-        return data;
-    }catch(error){
-        console.log("error");
-    }
-}
-
+    return data;
+  } catch (error) {
+    console.log("error");
+  }
+};
